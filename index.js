a9131c28c613245d963ab8cc86a915dec40aa641
Marc-Philippe Huget <mphuget@gmail.com>
2020-10-18 15:30:51 +0200

    First implementation for Heroku

---
+++ index.js
@@ -0,0 +1,21 @@
+//Use Express middleware to manage incoming requests and 
+//dispatch them to corresponding behaviours
+const express = require('express');
+
+//Create an application 
+const app = express();
+
+//Send back a raw message every time the server got an 
+//incoming request
+app.get('*', (req, res) => {
+
+    res.send('Express response');
+
+});
+
+//Listen on the port 3000
+app.listen(3000);
+
+//Print out where the server is
+console.log("Server is running on port: 3000");
+
